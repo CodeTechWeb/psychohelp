68d33edb8183d4c004b7ce8c78001b8142f94ba4
Alessandro Chumpitaz <par_214@hotmail.com>
2021-09-11 16:53:37 -0500

    Angular new app

---
+++ src/app/components/psychologists/psychologists.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-psychologists',
+  templateUrl: './psychologists.component.html'
+})
+export class PsychologistsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/types/psychologist.ts
@@ -0,0 +1,4 @@
+export interface Psychologist {
+  id: number;
+  name: string;
+}
