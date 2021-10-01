64422abe86368d3bae176a3f456c5263365650af
sebas1803 <52021716+sebas1803@users.noreply.github.com>
2021-10-01 15:39:30 -0500

    Added patient and psychologist profile

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -5,12 +5,16 @@ import { SignUpComponent } from "../pages/sign-up/sign-up.component";
 import { PageNotFoundComponent } from "../pages/common/page-not-found/page-not-found.component";
 import { HomeComponent } from "../pages/home/home.component";
 import { RoomComponent } from "../pages/room/room.component";
+import { PsychologistProfileComponent } from "../pages/psychologist-profile/psychologist-profile.component";
+import { PatientProfileComponent } from "../pages/patient-profile/patient-profile.component";
 
 const index: Routes = [
   { path: '', component: HomeComponent },
   { path: 'room/:id', component: RoomComponent },
   { path: 'login', component: SignInComponent },
   { path: 'register', component: SignUpComponent },
+  { path: 'psychoprofile', component: PsychologistProfileComponent },
+  { path: 'patientprofile', component: PatientProfileComponent },
   { path: 'psychologists', component: PsychologistsComponent },
   { path: '**', component: PageNotFoundComponent }
 ];
