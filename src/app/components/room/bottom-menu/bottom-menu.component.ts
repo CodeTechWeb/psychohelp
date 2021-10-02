d19615dbc88cd940828cc8e2087df6f27ef09814
Alessandro <par_214@hotmail.com>
2021-10-02 02:14:08 -0500

    Edited Room Layout

--- src/app/components/room/bottom-menu/bottom-menu.component.ts
+++ src/app/components/room/bottom-menu/bottom-menu.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
+type Option = {
+  icon: string;
+  label: string;
+  color: {
+    button: string;
+    icon: string;
+  };
+}
 @Component({
   selector: 'app-bottom-menu',
   templateUrl: './bottom-menu.component.html'
 })
 export class BottomMenuComponent implements OnInit {
 
+  options: Array<Option> = [
+    { icon: 'mic_off', color: { button: 'primary', icon: 'basic'}, label:'microphone' },
+    { icon: 'videocam_off', color: { button: 'primary', icon: 'basic'}, label: 'video camera' },
+    { icon: 'present_to_all', color: { button: 'primary', icon: 'basic'}, label: 'presentation'},
+    { icon: 'more_vert', color: { button: 'primary', icon: 'basic'}, label: 'options'},
+    { icon: 'call_end', color: { button: 'warn', icon: 'basic'}, label: 'hang'}
+  ]
   constructor() { }
 
   ngOnInit(): void {
