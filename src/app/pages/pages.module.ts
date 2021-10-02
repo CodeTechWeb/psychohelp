87bfa2c9bc7bfd5a54efb44ec87f2d8a4c2c9a05
sebas1803 <52021716+sebas1803@users.noreply.github.com>
2021-10-01 21:50:51 -0500

    feat: added patient and psychologist forms

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -4,6 +4,9 @@ import { SignUpComponent } from "./sign-up/sign-up.component";
 import { PsychologistsComponent } from "./psychologists/psychologists.component";
 import { RoomComponent } from "./room/room.component";
 import { PageNotFoundComponent } from "./common/page-not-found/page-not-found.component";
+import {PatientProfileComponent} from "./patient-profile/patient-profile.component";
+import {PsychologistProfileComponent} from "./psychologist-profile/psychologist-profile.component";
+
 
 import { ComponentsModule } from "../components/components.module";
 import { LayoutModule } from "../components/layout/layout.module";
@@ -13,10 +16,15 @@ import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
 import {MatTableModule} from "@angular/material/table";
 import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from "@angular/material/card";
+import {MatDatepickerModule} from "@angular/material/datepicker";
 import { HomeComponent } from './home/home.component';
 import {CommonModule} from "@angular/common";
 import {SocketIoConfig, SocketIoModule} from "ngx-socket-io";
 import {environment} from "../../environments/environment";
+
+
 
 //@ts-ignore
 const config: SocketIoConfig = {url: environment.socketUrl, options: {withCredentials: '*'}};
@@ -27,6 +35,8 @@ const config: SocketIoConfig = {url: environment.socketUrl, options: {withCreden
     SignUpComponent,
     PsychologistsComponent,
     PageNotFoundComponent,
+    PatientProfileComponent,
+    PsychologistProfileComponent,
     RoomComponent,
     HomeComponent,
   ],
@@ -40,8 +50,11 @@ const config: SocketIoConfig = {url: environment.socketUrl, options: {withCreden
     MatIconModule,
     MatTableModule,
     MatPaginatorModule,
+    MatGridListModule,
     CommonModule,
-    SocketIoModule.forRoot(config)
+    SocketIoModule.forRoot(config),
+    MatCardModule,
+    MatDatepickerModule
   ]
 })
 export class PagesModule { }
--- src/app/pages/patient-profile/patient-profile.component.ts
+++ src/app/pages/patient-profile/patient-profile.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-patient-profile',
   templateUrl: './patient-profile.component.html',
-  styleUrls: ['./patient-profile.component.css']
 })
 export class PatientProfileComponent implements OnInit {
 
