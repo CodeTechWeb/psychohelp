9f9b4f8a7301577bc4ea0f6f7fe0d2a570b72a05
Debie García de la Cruz Melo <debiegarcia@hotmail.com>
2021-10-01 21:46:06 -0500

    Update Home Component

--- src/app/services/publications.service.ts
+++ src/app/services/publications.service.ts
@@ -1,15 +1,15 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {HttpClient, HttpErrorResponse, HttpHeaders} from "@angular/common/http";
+import {Observable, throwError} from "rxjs";
 import {Publications} from "../types/publications";
-
-
+import {catchError, retry} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
-export class PublicationsService {
 
+
+export class PublicationsService {
 
   httpOptions = {
     headers: new HttpHeaders({
@@ -19,9 +19,41 @@ export class PublicationsService {
 
 
   private apiURL = 'http://localhost:8080/publications';
-  constructor(private http: HttpClient) { }
-  getPublications(): Observable<any> {
-    return this.http.get<Publications[]>(this.apiURL);
+  constructor(private http: HttpClient) {
 
   }
+
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      //Default error handling
+      console.log(`An error occurred: ${error.error.message}`);
+    } else {
+      //Unsuccessful Response Error Code returned from Backend
+      console.error(
+        `Backend returned code ${error.status}, body was: ${error.error}`
+      );
+    }
+    // Return Observable with Error Message to Client
+    return throwError('Something happened with request, please try again later');
+  }
+
+  getPublications(): Observable<any> {
+    return this.http.get<Publications[]>(this.apiURL)
+      .pipe(
+        retry(2),
+        catchError(this.handleError));
+  }
+
+  addPublications(publication: Publications){
+    return this.http.post(this.apiURL, JSON.stringify(publication), this.httpOptions)
+      .pipe(
+        retry(2),
+        catchError(this.handleError)
+      );
+
+  }
+
+
+
+
 }
