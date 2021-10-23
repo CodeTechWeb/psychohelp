ef13f6d97c82d525eb625eab5180ec4dfc48e9ee
Jose Tarazona <joset2804@hotmail.com>
2021-10-23 16:54:51 -0500

    Update sign-in

--- src/app/pages/patient/sign-in/sign-in.component.ts
+++ src/app/pages/patient/sign-in/sign-in.component.ts
@@ -1,14 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html'
 })
 export class SignInComponent implements OnInit {
-  hide = true;
-  constructor() { }
 
   ngOnInit(): void {
   }
+
+  emailFormControl = new FormControl('', [
+    Validators.required,
+    Validators.email,
+  ]);
+
+  hide = true;
 
 }
