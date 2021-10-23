b966ef017c88e0dc5d2b6931a8dd56212bbb84cb
JosePainP <67244047+ThePain99@users.noreply.github.com>
2021-10-23 17:39:54 -0500

    Update psychologist-route

--- src/app/routes/psychologist.ts
+++ src/app/routes/psychologist.ts
@@ -1,5 +1,9 @@
 import {PsychologistsComponent} from "../pages/patient/psychologists/psychologists.component";
+import {RegisterComponent} from "../pages/psychologist/register/register.component";
+import {LoginComponent} from "../pages/psychologist/login/login.component";
 
 export default [
   { path: 'psychologist/home/:id', component: PsychologistsComponent },
+  { path: 'psychologistRegister', component: RegisterComponent },
+  { path: 'psychologistLogin', component: LoginComponent },
 ]
