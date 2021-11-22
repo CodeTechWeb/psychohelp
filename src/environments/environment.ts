07852afb16a0db36bb7ff7bfbf5f407a93369969
Jose Tarazona <joset2804@hotmail.com>
2021-11-22 12:55:09 -0500

    Fix Environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   socketUrl: 'http://localhost:3000',
-  apiUrl: 'http://localhost:8080/api/v1',
+  apiUrl: 'http://psychohelp-open.mybluemix.net/api/v1',
   peer: { host: 'localhost', port: 3001 },
   stripeKey: ''
 };
