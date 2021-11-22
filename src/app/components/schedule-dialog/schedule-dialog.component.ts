e1cd68cc6fcc3f3529de619fedb62b3faf96816e
Jose Tarazona <joset2804@hotmail.com>
2021-11-22 12:54:36 -0500

    Update Schedule

---
+++ src/app/components/schedule-dialog/schedule-dialog.component.ts
@@ -0,0 +1,42 @@
+import {Component, Inject, OnInit} from '@angular/core';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {PsychologistsComponent} from "../../pages/patient/psychologists/psychologists.component";
+import {PsychologistService} from "../../services/psychologist.service";
+import psychologist from "../../routes/psychologist";
+
+@Component({
+  selector: 'app-schedule-dialog',
+  templateUrl: './schedule-dialog.component.html',
+})
+export class ScheduleDialogComponent implements OnInit {
+  receivedPsychologist:any;
+  receivedPatient: any;
+  schedules!: any;
+
+
+  constructor(public dialogRef: MatDialogRef<PsychologistsComponent>, private psychologist: PsychologistService,
+              @Inject(MAT_DIALOG_DATA) public data:any) {
+    this.receivedPsychologist=data;
+    this.receivedPatient=data;
+  }
+
+  closeDialog() {
+    this.dialogRef.close();
+  }
+
+  retrieveSchedule(){
+    console.log(this.receivedPsychologist.id)
+    this.psychologist.findPsychologistSchedule(this.receivedPsychologist.id).subscribe(
+      (data) => {
+        this.schedules = data;
+        console.log(this.schedules);
+      }
+    );
+  }
+
+  ngOnInit(): void {
+
+    this.retrieveSchedule();
+  }
+
+}
